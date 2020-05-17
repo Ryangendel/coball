b551bc264abe39ea09a1795b4758938d03d87366
HartleyDavid <dhartley@uams.edu>
2020-05-17 17:03:01 -0500

    Now with a working gmailer

--- Mailer.py
+++ Mailer.py
@@ -20,26 +20,35 @@ sslContext = ssl.create_default_context()
 # <b>This is HTML message.</b>
 # <h1>This is headline.</h1>
 # """
-class Message():
-    def __init__(self,sender: str = "No-Reply@website.com", receiver: list = app.config["ADMIN_MAIL_LIST"], message: str = None) -> None:
-        self.sender = sender
-        self.receiver = receiver
-        self.message = message
+# from flask_mail import Mail,Message
+# class Message():
+#     def __init__(self,sender: str = "No-Reply@website.com", receiver: list = app.config["ADMIN_MAIL_LIST"], message: str = None) -> None:
+#         self.sender = sender
+#         self.receiver = receiver
+#         self.message = message
         
 
-class Mailer():
-    def __init__(self) -> None:
-        self.smtpHost = smtpHost
-        self.smtpPort = smtpPort
+# class Mailer():
+#     def __init__(self) -> None:
+#         self.smtpHost = smtpHost
+#         self.smtpPort = smtpPort
     
-    def sendMail(self,message: Message) -> bool:
-        try:
-            with smtplib.SMTP_SSL(self.smtpHost, self.smtpPort, context=sslContext) as server:
-                server.login(mailUserName, mailUserPW)
-                for recipients in message.receiver:
-                    server.sendmail(message.sender,message.receiver,message.message)
-            return True
-        except Exception as e:
-            print(e)
-            return False
+#     def sendMail(self,message: Message) -> bool:
+#         try:
+#             with smtplib.SMTP_SSL(self.smtpHost, self.smtpPort, context=sslContext) as server:
+#                 server.login(mailUserName, mailUserPW)
+#                 for recipients in message.receiver:
+#                     server.sendmail(message.sender,message.receiver,message.message)
+#             return True
+#         except Exception as e:
+#             print(e)
+#             return False
 
+# mail_settings = {
+#     "MAIL_SERVER": app.config['SMTP_HOST'],
+#     "MAIL_PORT": app.config['SMTP_PORT'],
+#     "MAIL_USE_TLS": False,
+#     "MAIL_USE_SSL": True,
+#     "MAIL_USERNAME": app.config['MAIL_USER'],
+#     "MAIL_PASSWORD": app.config['MAIL_USER_PW']
+# }
