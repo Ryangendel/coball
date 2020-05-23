58f3bb3b1610db5c147c33b9b59f0e89654cfd90
HartleyDavid <dhartley@uams.edu>
2020-05-23 14:13:51 -0500

    updated db string

--- config.py
+++ config.py
@@ -20,5 +20,6 @@ class Qa(Config):
 
 class Production(Config):
     DEBUG = False
-    DATABASE_URL = 'postgres://iuhpxmphwetgmb:6dac26780c25f010c474c12973f76014d6f309eb2085f37cdb43eeafc1da9bfc@ec2-52-202-146-43.compute-1.amazonaws.com:5432/dc55qk2dqth8s7'
+    # DATABASE_URL = 'postgres://iuhpxmphwetgmb:6dac26780c25f010c474c12973f76014d6f309eb2085f37cdb43eeafc1da9bfc@ec2-52-202-146-43.compute-1.amazonaws.com:5432/dc55qk2dqth8s7'
+    DATABASE_URL = 'postgres://nvcovycexeihqa:33da3f5c0e1ebbea454897cb9461636e417265254ca78186144c2a85b7939585@ec2-34-198-243-120.compute-1.amazonaws.com:5432/d63e1srt1untk7'
     # SERVER_NAME ='prod'
